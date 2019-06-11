ab42cf8fd02bf2e8a63f08484eb8f4b8316c23ef
Brandon Day <brandontday7@gmail.com>
2019-06-10 20:10:10 -0400

    no bug when data.json is empty

--- src/Histogram.js
+++ src/Histogram.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Options from "./Options"
 import { Bar } from "react-chartjs-2"
 import jsonData from "./dataUtility/data.json"
+import _ from "lodash"
 import { generateData, capitalize } from "./utils/utilities.js"
 
 class Chart extends Component {
@@ -9,7 +10,7 @@ class Chart extends Component {
 		super(props)
 		this.state = {
 			feature: "temperature",
-			dataPoints: jsonData.temperature,
+			dataPoints: jsonData[Object.keys(jsonData)[0]],
 			scale: "daily"
 		}
 	}
@@ -21,43 +22,46 @@ class Chart extends Component {
 	changeFeature = feature => this.setState({ feature, dataPoints: jsonData[feature] })
 
 	render() {
-		const { feature, dataPoints, scale } = this.state
-		let rawData = generateData(dataPoints, scale)
-		let dateLabels = rawData.map(point => point.x)
+		if (_.isEmpty(jsonData)) return <p>No data</p>
+		else {
+			const { feature, dataPoints, scale } = this.state
+			let rawData = generateData(dataPoints, scale)
+			let dateLabels = rawData.map(point => point.x)
 
-		let data = {
-	    labels: dateLabels,
-	    datasets: [
-	      {
-	        label: capitalize(feature),
-	        data: rawData,
-	      }
-	    ]
+			let data = {
+		    labels: dateLabels,
+		    datasets: [
+		      {
+		        label: capitalize(feature),
+		        data: rawData,
+		      }
+		    ]
+			}
+
+			return (
+				<div>
+					<Bar
+					  data={data}
+					  options={{ 
+					  	maintainAspectRatio: false, scales: {
+			        yAxes: [{
+			            display: true,
+			            ticks: {
+			              suggestedMin: 0
+			            }
+			        }]}
+			      }}
+					/>
+					<Options 
+						changeScale={this.changeScale} 
+						changeFeature={this.changeFeature} 
+						featureOptions={Object.keys(jsonData)}
+					/>
+				</div>
+			)
 		}
-		console.log(jsonData)
 
-		return (
-			<div>
-				<Bar
-				  data={data}
-				  options={{ 
-				  	maintainAspectRatio: false, scales: {
-		        yAxes: [{
-		            display: true,
-		            ticks: {
-		              suggestedMin: 0
-		            }
-		        }]}
-		      }}
-				/>
-				<Options 
-					changeScale={this.changeScale} 
-					changeFeature={this.changeFeature} 
-					featureOptions={Object.keys(jsonData)}
-				/>
-			</div>
-		)
-	}
+		}
 
 }
 
